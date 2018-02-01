61133cf388f3e119e70f17d116961688286db712
Benjamin Drung <benjamin.drung@profitbricks.com>
2018-02-01 15:05:13 +0100

    python_tests: Bump copyright year

--- python_tests/test_flake8.py
+++ python_tests/test_flake8.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017, Benjamin Drung <bdrung@debian.org>
+# Copyright (C) 2017-2018, Benjamin Drung <bdrung@debian.org>
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
--- python_tests/test_pylint.py
+++ python_tests/test_pylint.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2010, Stefano Rivera <stefanor@debian.org>
-# Copyright (C) 2017, Benjamin Drung <bdrung@debian.org>
+# Copyright (C) 2017-2018, Benjamin Drung <bdrung@debian.org>
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
